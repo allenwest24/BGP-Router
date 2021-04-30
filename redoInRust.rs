38bf6cd153edf7bb51ca0124ae4d8a0dc68eff7f
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-04-30 16:46:32 -0400

    Implemented the Router class without field formatting.

--- redoInRust.rs
+++ redoInRust.rs
@@ -1,7 +1,10 @@
+// Imports used.
 use std::env;
+use std::collections::HashMap;
 
+// The Router class used to simulate a BGP router.
 struct Router {
-  networks: Vec<String>,
+  networks: HashMap<String, String>,
   routes: Vec<String>,
   updates: Vec<String>,
   relations: Vec<String>,
@@ -9,23 +12,32 @@ struct Router {
   asn: String,
 }
 
+// Methods used to implement the functionality of our Router.
 impl Router {
-  pub fn new(networks: Vec<String>) -> Self {
+  pub fn new(networks: HashMap<String, String>, asn: String) -> Self {
     Self {
       networks,
       routes : Vec::new(),
       updates : Vec::new(),
       relations : Vec::new(),
       sockets : Vec::new(),
-      asn : "127.0.0.1".to_string(),
+      asn,
     }
   }
 }
 
+// The main method for this router program.
 fn main() {
-  println!("Hello, world!");
-  let args: Vec<String> = env::args().collect();
-  println!("{:?}", args);
-  let r: Router = Router::new(args);
-  //r.run()
+  // Parse the arguments.
+  let mut args: Vec<String> = env::args().collect();
+  // The asn is the first arguments after the program call.
+  let mut asn : String = args.remove(1);
+  // The rest of the arguments should be relationships.
+  let mut networks = HashMap::new();
+  for ii in 1..args.len() {
+    networks.insert(args.get(ii).unwrap().into(), (ii + 1).to_string());
+  }
+  
+  // Initialize our router.
+  let r: Router = Router::new(networks, asn);
 }
